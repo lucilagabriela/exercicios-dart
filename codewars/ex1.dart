3683faaba169e49bb99a4c7316d40034f255c941
Lucila Costa <lucilagdac@gmail.com>
2023-10-10 18:50:15 -0300

    up

---
+++ codewars/ex1.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  //String? quadrado = stdin.readLineSync();
+  String? quadrado = int.parse(stdin.readLineSync());
+
+  if (quadrado)
+}
--- estrutura.dart
+++ estrutura.dart
@@ -2,17 +2,46 @@
 // cada objeto é uma instancia de uma classe
 // todos os objetos herdam da classe Object
 
-// declaracao de variaveis:
+
 void main() {
+// declaracao de variaveis:
   var nome = 'Lucila'; // string
   var idade = 21; // int
   var peso = 55.5; // double
   var verdade = true; // bool
-
-  Object nomeCompleto = 'Lucila Gabriela';
+  String namee = 'Costa';
+  Object nomeCompleto = 'Lucila Gabriela'; // objetivo não restrito a um unico tipo
   String nomeComp = 'Lucila Costa';
   // dynamic
-  
-
   print(nome);
+
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+
+// estrutura condicional
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+// estrutura de repeticao
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
 }
