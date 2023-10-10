44ef2341f5c52a0c2b3ccc9e82a5ce08ec3bd903
Lucila Costa <lucilagdac@gmail.com>
2023-10-10 16:12:26 -0300

    Create estrutura.dart

---
+++ estrutura.dart
@@ -0,0 +1,18 @@
+// tudo o que se pode colocar em uma variavel, é um objeto
+// cada objeto é uma instancia de uma classe
+// todos os objetos herdam da classe Object
+
+// declaracao de variaveis:
+void main() {
+  var nome = 'Lucila'; // string
+  var idade = 21; // int
+  var peso = 55.5; // double
+  var verdade = true; // bool
+
+  Object nomeCompleto = 'Lucila Gabriela';
+  String nomeComp = 'Lucila Costa';
+  // dynamic
+  
+
+  print(nome);
+}
